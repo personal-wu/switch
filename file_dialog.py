8debb1d272bda6633a2cf5a062d8cfa765107640
Jonny.Wu <personal_wu@hotmail.com>
2016-03-24 11:24:51 +0800

    将filedialog.py改成file_dialog.py，并且将其中窗口初始化过程封装在InitWnd方法中

---
+++ file_dialog.py
@@ -0,0 +1,35 @@
+#__author__ = 'Jonny'
+#coding=utf-8
+
+import os
+import sys
+import Tkinter
+import tkFileDialog
+from Tkinter import *
+
+root = Tk()
+def selectFiles():
+    fnameEncode = tkFileDialog.askopenfilenames(filetypes=[('Excel file', '*.xls;*.xlsx')], multiple=1)
+    #sysencode = sys.getfilesystemencoding()
+    #fnameDecode = fnameEncode.encode(sysencode)
+    #fnameTmpArray = root.tk.splitlist(fnameEncode)
+    #fnameArray = [unicode(i, encoding=sysencode) for i in fnameTmpArray]
+    print fnameEncode
+
+def saveFiles():
+    fnameSaved = tkFileDialog.asksaveasfilename(filetypes=[('Excel file', '*.xls;*.xlsx'),('Text document file','*.txt'),('XML file',';*.xml')])
+    print fnameSaved
+
+def InitWnd():
+    root.title("文件转换...")
+    root.geometry("300x150")
+    root.iconbitmap("res/128x128.ico")
+    buttonSelect = Button(root, text ="选择文件..", command = selectFiles)
+    buttonSelect.pack()
+
+    buttonSave = Button(root, text ="保存..", command = saveFiles)
+    buttonSave.pack()
+    root.mainloop()
+
+if __name__ == "__main__":
+    InitWnd()
